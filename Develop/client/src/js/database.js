486a4a028b7c292a65340d974265085bcbc5ea63
nrasch86 <145396887+nrasch86@users.noreply.github.com>
2024-03-23 15:53:30 -0400

    Updated database.js methods

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const result = await store.put({ content }); // Add content
+    console.log('Data saved to the database', result); // Log success
+    return result; // Return the result
+  } catch (error) {
+    console.error('Error saving data to the database', error); // Log any errors
+    throw error;
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const result = await store.getAll(); // Fetch Data
+    console.log('Data retrieved from the database', result);
+    return result; // Return data
+  } catch (error) {
+    console.error('Error retrieving data from the database', error); // Log errors
+    throw error;
+  }
+};
 
 initdb();
